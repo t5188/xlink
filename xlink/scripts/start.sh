42f5e11530e83ec6dbbe1e890ebbfddb5e11fa82
coomsn <coomsn@qq.com>
2024-12-31 00:58:37 +0800

    Update start.sh

--- xlink/scripts/start.sh
+++ xlink/scripts/start.sh
@@ -5,13 +5,13 @@ scripts_dir=$(dirname ${scripts})
 parent_dir=$(dirname ${scripts_dir})
 module_dir="/data/adb/modules/xlink"
 
+cd ${scripts_dir}
 source ${scripts_dir}/xlink.service
-log Info "The process is starting, please wait"
 
-# Check if the disable file does not exist, then run the proxy
+log Info "The process is starting, please wait"
 if [ ! -f "${module_dir}/disable" ]; then
   start_tproxy # >/dev/null 2>&1
-  else
+else
   log Warn "module is not enabled"
 fi
 
@@ -20,6 +20,7 @@ start_xlink.inotify() {
   for PID in "${PIDs[@]}"; do
     if grep -q "xlink.inotify" "/proc/$PID/cmdline"; then
       kill -9 "$PID"
+      return
     fi
   done
   inotifyd "${scripts_dir}/xlink.inotify" "${module_dir}" >/dev/null 2>&1 &
